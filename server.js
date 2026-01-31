bcd6f4c9113cc46e108c77b28077e217561fe390
Siddhesh Alhat <siddesh5432alhat@gmail.com>
2026-01-31 19:13:53 +0530

    Log code

--- server.js
+++ server.js
@@ -174,5 +174,10 @@ app.post('/register-repairman', async (req, res) => {
 
 // Start Server
 // const PORT = process.env.PORT || 3000;
-const PORT = process.env.PORT || 10000 ;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+// const PORT = process.env.PORT || 10000 ;
+// app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
